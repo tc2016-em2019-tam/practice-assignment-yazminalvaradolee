1492c584725906c76caa9a954d39717afd5ef945
Yazmin <yazminalvlee@gmail.com>
2019-01-20 23:57:34 -0600

    initial commit

---
+++ com/Yasmin/Main.java
@@ -0,0 +1,13 @@
+package com.Yasmin;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        MegabyteConverter.printMegaBytesAndKiloBytes(4100);
+
+        MegabyteConverter.printMegaBytesAndKiloBytes(2055);
+
+        MegabyteConverter.printMegaBytesAndKiloBytes(3072);
+    }
+}
